8cf95283ed8efce202d1795e53ef40d113613bd0
Adolfo Rangel <109772815+Adolfo-Rangel@users.noreply.github.com>
2025-01-16 13:57:04 -0800

    Create Node.java

---
+++ CS282ADS/bstImplementation/Node.java
@@ -0,0 +1,15 @@
+package com.mycompany.bstimpl;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(){
+        
+    }
+    public Node(int data){
+        this.data = data;
+        
+    }
+}
